a2c18fb497edf62d4b8e8972b8753d6ea3885a09
Brendon Angelo Rodrigues <brendonangelo@hotmail.com>
2021-09-21 02:47:13 -0300

    Responsividade arrumada e animação adicionada no login page

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -17,6 +17,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Pay Flow",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           canvasColor: Colors.transparent,
           primaryColor: AppColors.primary,
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,11 +1,11 @@
+import 'package:animated_card/animated_card.dart';
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
-import 'package:nlw_project/models/user_model.dart';
 import 'package:nlw_project/modules/login/login_controller.dart';
 import 'package:nlw_project/themes/app_colors.dart';
 import 'package:nlw_project/themes/app_images.dart';
 import 'package:nlw_project/themes/app_text_style.dart';
 import 'package:nlw_project/widgets/social_login/social_login_button.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -32,72 +32,109 @@ class _LoginPageState extends State<LoginPage> {
               height: size.height * .36,
               color: AppColors.primary,
             ),
-            Positioned(
-              top: 40,
-              left: 0,
-              right: 0,
-              child: Container(
-                child: Image.asset(
-                  AppImages.person,
-                  width: 208,
-                  height: 310,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 260,
-              left: 0,
-              right: 0,
-              child: Container(
-                decoration: BoxDecoration(
-                  // color: Colors.white,
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Colors.white.withOpacity(0.0),
-                      Colors.white,
-                    ],
-                    stops: [0.0, 0.8],
-                  ),
-                ),
-                // width: 206,
-                height: 96,
-              ),
-            ),
-            Positioned(
-              bottom: size.height * .05,
-              right: 0,
-              left: 0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(AppImages.logomini),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 70,
-                      right: 70,
-                      top: 24,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 70),
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: size.height * .05, bottom: 10),
+                      child: Container(
+                        height: 315,
+                        child: Stack(
+                          children: [
+                            AnimatedCard(
+                              direction: AnimatedCardDirection.bottom,
+                              curve: Curves.easeInOut,
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Image.asset(
+                                  AppImages.person,
+                                  height: 310,
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      Colors.white.withOpacity(0.0),
+                                      Colors.white,
+                                    ],
+                                    stops: [0, 0.8],
+                                  ),
+                                ),
+                                height: 310 * .28,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    child: Text(
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 24),
+                      child: Image.asset(AppImages.logomini),
+                    ),
+                    Text(
                       "Organize seus boletos em um só lugar",
                       style: TextStyles.titleHome,
                       textAlign: TextAlign.center,
                     ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 40, right: 40, top: 40),
-                    child: SocialLoginButton(
-                      onTap: () async {
-                        controller.googleSignIn(context);
-                      },
-                    ),
-                  )
-                ],
+                  ],
+                ),
               ),
             ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(40),
+                child: SocialLoginButton(
+                  onTap: () async {
+                    controller.googleSignIn(context);
+                  },
+                ),
+              ),
+            ),
+            // Positioned(
+            //   bottom: size.height * .05,
+            //   right: 0,
+            //   left: 0,
+            //   child: Column(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     crossAxisAlignment: CrossAxisAlignment.center,
+            //     children: [
+            //       Image.asset(AppImages.logomini),
+            //       Padding(
+            //         padding: EdgeInsets.only(
+            //           left: 70,
+            //           right: 70,
+            //           top: 24,
+            //         ),
+            //         child: Text(
+            //           "Organize seus boletos em um só lugar",
+            //           style: TextStyles.titleHome,
+            //           textAlign: TextAlign.center,
+            //         ),
+            //       ),
+            //       Padding(
+            //         padding:
+            //             const EdgeInsets.only(left: 40, right: 40, top: 40),
+            //         child: SocialLoginButton(
+            //           onTap: () async {
+            //             controller.googleSignIn(context);
+            //           },
+            //         ),
+            //       )
+            //     ],
+            //   ),
+            // ),
           ],
         ),
       ),
